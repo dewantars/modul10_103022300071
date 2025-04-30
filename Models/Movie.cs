b9c215ce481d484cf32387f49390ff285f8bb46a
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-30 16:39:02 +0700

    menambahkan API Movies

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,5 +6,10 @@
         public string Director { get; set; }
         public List<string> Stars { get; set; }
         public string Description { get; set; }
+
+        public Movie()
+        {
+            Stars = new List<string>();
+        }
     }
 }
